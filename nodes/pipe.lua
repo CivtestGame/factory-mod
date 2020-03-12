70a8e8de8bb2df2f8a7d2e9b794148d868188441
igotyou <>
2020-03-12 12:53:40 +0100

    Small change to reflect network mod update

--- nodes/pipe.lua
+++ nodes/pipe.lua
@@ -4,7 +4,6 @@ minetest.register_node(name, {
     description = "Pipe",
     tiles = {"^[colorize:#3248a8"},
     groups = {choppy = 2, oddly_breakable_by_hand = 2, wood = 1},
-    on_place = NodeNetwork.citadel_network.before_node_place,
     on_rightclick = function(pos, node, player, itemstack, pointed_thing)
         local network = NodeNetwork.IO_network(pos, "steam")
         network:update_infotext()
